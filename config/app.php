b947421a77b28a14e0839f42503ae6d4f24a2f20
VladislavNep <VladislavNep@yandex.ru>
2019-10-16 22:27:42 +0700

    fix

--- config/app.php
+++ config/app.php
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+//    'url' => env('APP_URL', 'http://localhost'),
+    'url' =>  'https://voicevkbot.herokuapp.com/',
 
     'asset_url' => env('ASSET_URL', null),
 
