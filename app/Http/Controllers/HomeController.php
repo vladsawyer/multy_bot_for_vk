75ae7c0e52c355a56860df0eac1f46b0a2838d73
VladislavNep <VladislavNep@yandex.ru>
2019-10-18 15:40:55 +0700

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,54 +36,52 @@ class HomeController extends Controller
         $vk_callback_event = json_decode(file_get_contents("php://input"));
 
 
-        return 'ok';
-//
-//
-//        try{
-//            if ($vk_callback_event['secret'] !== getenv('VK_SECRET_TOKEN')) {
-//                return 'nioh';
-//
-//            }
-//
-//            switch ($vk_callback_event['type']){
-//                case 'confirmation':
-//                    return getenv('VK_CONFIRMATION_CODE');
-//                    break;
-//
-//                case 'message_new':
-//                    // получил id отправителя сообщения
-//                    $user_id = $vk_callback_event['object'] -> from_id;
-//                    $txt = $vk_callback_event['object'] -> text;
-//
-//                    // получаю его имя
-//                    $vk = new VKApiClient();
-//                    $response = $vk->users()->get(getenv('VK_TOKEN'), array(
-//                        'user_ids' => [$user_id],
-//                    ));
-//                    $name = $response[0]['first_name'];
-//
-//                    if ($txt === ('привет' || 'начать' )){
-//                        // отправляем сообщение приветствие
-//                        $vk = new VKApiClient();
-//                        $response = $vk->messages()->send(getenv('VK_TOKEN'), array(
-//                            'user_id' => $user_id,
-//                            'message' => "Добро пожаловать Милорд $name",
-//                        ));
-//                    }
-//
-//                    return 'ok';
-//                    break;
-//                default:
-//                    return 'ok';
-//                    break;
-//            }
-//
-//        } catch (\Exception $ex) {
-//            //Выводим сообщение об исключении.
-//            echo $ex->getMessage();
-//        }
-//
-//
+
+        try{
+            if ($vk_callback_event['secret'] !== getenv('VK_SECRET_TOKEN')) {
+                return 'nioh';
+
+            }
+
+            switch ($vk_callback_event['type']){
+                case 'confirmation':
+                    return getenv('VK_CONFIRMATION_CODE');
+                    break;
+
+                case 'message_new':
+                    // получил id отправителя сообщения
+                    $user_id = $vk_callback_event['object'] -> from_id;
+                    $txt = $vk_callback_event['object'] -> text;
+
+                    // получаю его имя
+                    $vk = new VKApiClient();
+                    $response = $vk->users()->get(getenv('VK_TOKEN'), array(
+                        'user_ids' => [$user_id],
+                    ));
+                    $name = $response[0]['first_name'];
+
+                    if ($txt === ('привет' || 'начать' )){
+                        // отправляем сообщение приветствие
+                        $vk = new VKApiClient();
+                        $response = $vk->messages()->send(getenv('VK_TOKEN'), array(
+                            'user_id' => $user_id,
+                            'message' => "Добро пожаловать Милорд $name",
+                        ));
+                    }
+
+                    return 'ok';
+                    break;
+                default:
+                    return 'ok';
+                    break;
+            }
+
+        } catch (\Exception $ex) {
+            //Выводим сообщение об исключении.
+            echo $ex->getMessage();
+        }
+
+
     }
 
 
