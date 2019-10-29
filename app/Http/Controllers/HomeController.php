34812fbeb8f7e903dc8f28428b5fad42f467cfaa
VladislavNep <VladislavNep@yandex.ru>
2019-10-29 23:28:16 +0700

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -59,40 +59,32 @@ class HomeController extends Controller
                                 'message' => "Добро пожаловать $name \n Вот список команд: \n 1) что умеешь",
                                 'random_id' => $random_id,
                             ));
-
+                            echo 'ok';
+                            break;
                         }
-                        if ($txt == ("Привет" || "Начать" || "привет") && $conversation_message_id != 0){
-                            // отправляем сообщение приветствие c возвращением
-                            $vk = new VKApiClient('5.101');
-                            $response = $vk->messages()->send(getenv('VK_TOKEN'), array(
-                                'user_id' => $user_id,
-                                'message' => "С возвращением $name \n Вот список команд: \n 1) что умеешь",
-                                'random_id' => $random_id,
-                            ));
-
-                        }
-                        if ($txt == "что умеешь" || "Что умеешь"){
-                            // отправляем сообщение
+//                        elseif ($txt == ("Привет" || "Начать" || "привет") && $conversation_message_id > 2){
+//                            // отправляем сообщение приветствие c возвращением
+//                            $vk = new VKApiClient('5.101');
+//                            $response = $vk->messages()->send(getenv('VK_TOKEN'), array(
+//                                'user_id' => $user_id,
+//                                'message' => "С возвращением $name \n Вот список команд: \n 1) что умеешь",
+//                                'random_id' => $random_id,
+//                            ));
+//                            echo 'ok';
+//                            break;
+//                        }
+                        elseif ($txt == ("что умеешь" || "Что умеешь") && $conversation_message_id > 2){
+                            // отправляем сообщение приветствие
                             $vk = new VKApiClient('5.101');
                             $response = $vk->messages()->send(getenv('VK_TOKEN'), array(
                                 'user_id' => $user_id,
                                 'message' => "$name скоро допилится функционал и вы сможите: \n 1) переводить текст в голосовые сообщения \n 2) менять голос бота \n 3) добавлять бота в чаты и переводить голосовые сообщения в текст \n 5) возможно многофункциональный переводчик, но это не точно:) \n \n Доброго дня)",
                                 'random_id' => $random_id,
                             ));
-
-                        } else {
-                            // отправляем сообщение
-                            $vk = new VKApiClient('5.101');
-                            $response = $vk->messages()->send(getenv('VK_TOKEN'), array(
-                                'user_id' => $user_id,
-                                'message' => "Неверная команда! \n Вот список команд: \n 1) что умеешь",
-                                'random_id' => $random_id,
-                            ));
-
+                            echo 'ok';
+                            break;
                         }
 
-                        echo 'ok';
-                        break;
                     } catch (\VK\Exceptions\VKApiException $e){
                         $this -> getlog($e -> getMessage());
                     }
