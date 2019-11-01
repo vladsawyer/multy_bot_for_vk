632ab7ec0c8af2439f38d09dd7575f61a4f02506
VladislavNep <VladislavNep@yandex.ru>
2019-11-02 00:09:59 +0700

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,7 +41,7 @@ class HomeController extends Controller
                         $object = $vk_callback_event['object'];
                         $user_id = $object['from_id'];
                         $txt =  $object['text'];
-                        //$value_button =  $object['payload']['button'];
+                        $value_button =  $object['payload']['button'];
 
                         // получаю его имя
                         $vk = new VKApiClient('5.103');
@@ -185,45 +185,44 @@ class HomeController extends Controller
                             ];
 
 
-//                        switch ( $value_button){
-//                            case  "start" :
-//                                $message = "Добро пожаловать $name! \n Я Мульти голосовой бот, разработанный [vladislav_nep | Непомнящих Владиславом], у меня есть свой сайт, его найдете в ссылках. \n Что я умею: \n 1️⃣ переводить текст в голосовые сообщения \n 2️⃣ Менять голос \n 3️⃣ Переводить голосовые сообщения в текст \n 4️⃣ Добавлять в чаты для автоматического перевода голосовых сообщений в текст \n 5️⃣ Повесилить вас историей для! \n \n Надеюсь я вам помогу или доставлю удовольствие!";
-//                                $value_keyboard = $keyboard_index;
-//                                break;
-//
-//                            case  "speech_recognition" :
-////                                $message = "$name скоро допилится функционал и вы сможите: \n 1) переводить текст в голосовые сообщения \n 2) менять голос бота \n 3) добавлять бота в чаты и переводить голосовые сообщения в текст \n 5) возможно многофункциональный переводчик, но это не точно:) \n \n Доброго дня)";
-//                                $message = "В разработке";
-//                                $value_keyboard = $keyboard_speech_recognition;
-//                                break;
-//
-//                            case  "speech_synthesis" :
-//                                $message = "Синтез речи запущен, в разработке)";
-//                                $value_keyboard = $keyboard_speech_synthesis;
-//
-////                                return  redirect() -> route('Speech_SynthesisController@index');
-//                                break;
-//
-//                            case  "voice" :
-//                                $message = "в разработке";
-//                                $value_keyboard = $keyboard_speech_synthesis_voice;
-//                                break;
-//
-//                            case  "history_day" :
-//                                $message = "В разработке";
-//                                $value_keyboard = "";
-//                                break;
-//
-//                            case "back_index" :
-//                                $message = "";
-//                                $value_keyboard = $keyboard_index;
-//                                break;
-//
-//                            default:
+                        switch ( $value_button || $txt){
+                            case  "start" || "/start":
+                                $message = "Добро пожаловать $name! \n Я Мульти голосовой бот, разработанный [vladislav_nep | Непомнящих Владиславом], у меня есть свой сайт, его найдете в ссылках. \n Что я умею: \n 1️⃣ переводить текст в голосовые сообщения \n 2️⃣ Менять голос \n 3️⃣ Переводить голосовые сообщения в текст \n 4️⃣ Добавлять в чаты для автоматического перевода голосовых сообщений в текст \n 5️⃣ Повесилить вас историей для! \n \n Надеюсь я вам помогу или доставлю удовольствие!";
+                                $value_keyboard = $keyboard_index;
+                                break;
+
+                            case  "speech_recognition" :
+                                $message = "В разработке";
+                                $value_keyboard = $keyboard_speech_recognition;
+                                break;
+
+                            case  "speech_synthesis" :
+                                $message = "Синтез речи запущен, в разработке)";
+                                $value_keyboard = $keyboard_speech_synthesis;
+
+//                                return  redirect() -> route('Speech_SynthesisController@index');
+                                break;
+
+                            case  "voice" :
+                                $message = "в разработке";
+                                $value_keyboard = $keyboard_speech_synthesis_voice;
+                                break;
+
+                            case  "history_day" :
+                                $message = "В разработке";
+                                $value_keyboard = "";
+                                break;
+
+                            case "back_index" :
+                                $message = "";
+                                $value_keyboard = $keyboard_index;
+                                break;
+
+                            default:
                                 $message = "Я вас не понял! Почему? \n 1) Неверная команда \n 2) Слишком длинный текст для синтеза речи \n 3) Аудио длиннее 30 сек для распознования речи";
                                 $value_keyboard = $keyboard_index;
-//                            break;
-//                        }
+                            break;
+                        }
 
                             // отправляем сообщение
                             $vk = new VKApiClient('5.103');
