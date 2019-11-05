348b601c4d4183440b8e7518385c1f750c2d3380
VladislavNep <VladislavNep@yandex.ru>
2019-11-05 18:11:50 +0700

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -116,7 +116,7 @@ class HomeController extends Controller
                                             "action" => [
                                                 "type" => "text",
                                                 "payload" => json_encode(["button" => "voice", "parametr_1" => "voice_man"]),
-                                                "label" => "🗣 Мужчина"
+                                                "label" => "Мужчина"
                                             ],
                                             "color" => "positive"
                                         ],
@@ -124,7 +124,7 @@ class HomeController extends Controller
                                             "action" => [
                                                 "type" => "text",
                                                 "payload" => json_encode(["button" => "voice", "parametr_1" => "voice_woman"]),
-                                                "label" => "🗣 Женщина"
+                                                "label" => "Женщина"
                                             ],
                                             "color" => "positive"
                                         ]
@@ -197,73 +197,70 @@ class HomeController extends Controller
                         $name = $response[0]['first_name'];
 
 
-//                        switch ($value_button) {
-//                            case  "start" :
-//                                $message = "Добро пожаловать $name! \n Я Мульти голосовой бот, разработчик [vladislav_nep | Непомнящих Владислав], у меня есть свой сайт, его найдете в ссылках. \n Что я умею: \n 1️⃣ переводить текст в голосовые сообщения  \n 2️⃣ Менять голос \n 3️⃣ Переводить голосовые сообщения в текст \n 4️⃣ Добавлять в чаты для автоматического перевода голосовых сообщений в текст \n 5️⃣ Повесилить вас историей для! \n Если не видите кнопок, то используйте цифры как команды. \n \n Надеюсь я вам помогу или доставлю удовольствие!";
-//                                $send_value_keyboard = $keyboard_index;
-//                                break;
-//
-//                            case  "speech_recognition" :
-//                                $message = "Отправьте голосовое сообщение до 30 секунд! В разработке)";
-//                                $send_value_keyboard = $keyboard_speech_recognition;
-//                                break;
-//
-//                            case "speech_recognition_instructions":
-//                                $message = "Здесь будет инструкция, пока лень писать)";
-//                                $send_value_keyboard = $keyboard_speech_recognition;
-//                                break;
-//
-//                            case "back_index" :
-//                                $message = "";
-//                                $send_value_keyboard = $keyboard_index;
-//                                break;
-//
-//                            case  "speech_synthesis" :
-//                                $message = "Синтез речи запущен, в разработке)";
-//                                $send_value_keyboard = $keyboard_speech_synthesis;
-//                                break;
-//
-//                            case "voice" :
-//                                $message = "";
-//                                $send_value_keyboard = $keyboard_speech_synthesis_voice;
-//                                switch ($value_parametr_1){
-//                                    case "back_speech_synthesis":
-//                                        $message = "";
-//                                        $send_value_keyboard = $keyboard_speech_synthesis;
-//                                        break;
-//                                    case "voice_man":
-//                                        $message  = "Смена голоса будет доступна в последнию очередь, Выбран голос: Мужчина";
-//                                        break;
-//                                    case "voice_woman":
-//                                        $message  = "Смена голоса будет доступна в последнию очередь, Выбран голос: Женщина";
-//                                        break;
-//                                }
-//                                break;
-//
-//                                case  "history_day" :
-//                                    $message = "В разработке";
-//                                        $send_value_keyboard = "";
-//                                        break;
-//
-//
-//                                    default:
-//                                        $message = "Я вас не понял! Почему? \n 1) Команды осуществляются только при помощи кнопок \n 2) Слишком длинный текст для синтеза речи \n 3) Аудио длиннее 30 сек для распознования речи";
-//                                        $send_value_keyboard = $keyboard_index;
-//                                        break;
-//                                    }
-                        $send_value_keyboard = $keyboard_speech_synthesis_voice;
-                        $message = "тест". $payload['parametr_1'];
+                        switch ($value_button) {
+                            case  "start" :
+                                $message = "Добро пожаловать $name! \n Я Мульти голосовой бот, разработчик [vladislav_nep | Непомнящих Владислав], у меня есть свой сайт, его найдете в ссылках. \n Что я умею: \n 1️⃣ переводить текст в голосовые сообщения  \n 2️⃣ Менять голос \n 3️⃣ Переводить голосовые сообщения в текст \n 4️⃣ Добавлять в чаты для автоматического перевода голосовых сообщений в текст \n 5️⃣ Повесилить вас историей для! \n Если не видите кнопок, то используйте цифры как команды. \n \n Надеюсь я вам помогу или доставлю удовольствие!";
+                                $send_value_keyboard = $keyboard_index;
+                                break;
 
-                                        // отправляем сообщение
-                                        $vk = new VKApiClient('5.103');
-                                        $response = $vk->messages()->send(getenv('VK_TOKEN'), array(
-                                            'user_id' => $user_id,
-                                            'message' => $message,
-                                            'keyboard' => json_encode($send_value_keyboard),
-                                            'random_id' => rand(),
-                                        ));
-                                        echo 'ok';
+                            case  "speech_recognition" :
+                                $message = "Отправьте голосовое сообщение до 30 секунд! В разработке)";
+                                $send_value_keyboard = $keyboard_speech_recognition;
+                                break;
+
+                            case "speech_recognition_instructions":
+                                $message = "Здесь будет инструкция, пока лень писать)";
+                                $send_value_keyboard = $keyboard_speech_recognition;
+                                break;
+
+                            case "back_index" :
+                                $message = "";
+                                $send_value_keyboard = $keyboard_index;
+                                break;
+
+                            case  "speech_synthesis" :
+                                $message = "Синтез речи запущен, в разработке)";
+                                $send_value_keyboard = $keyboard_speech_synthesis;
+                                break;
+
+                            case "voice" :
+                                $message = "";
+                                $send_value_keyboard = $keyboard_speech_synthesis_voice;
+                                switch ($value_parametr_1){
+                                    case "back_speech_synthesis":
+                                        $message = "";
+                                        $send_value_keyboard = $keyboard_speech_synthesis;
                                         break;
+                                    case "voice_man":
+                                        $message  = "Смена голоса будет доступна в последнию очередь, Выбран голос: Мужчина";
+                                        break;
+                                    case "voice_woman":
+                                        $message  = "Смена голоса будет доступна в последнию очередь, Выбран голос: Женщина";
+                                        break;
+                                }
+                                break;
+
+                            case "history_day":
+                                $message = "В разработке";
+                                $send_value_keyboard = "";
+                                break;
+
+                            default:
+                                $message = "Я вас не понял! Почему? \n 1) Команды осуществляются только при помощи кнопок \n 2) Слишком длинный текст для синтеза речи \n 3) Аудио длиннее 30 сек для распознования речи";
+                                $send_value_keyboard = $keyboard_index;
+                                break;
+                        }
+
+                        // отправляем сообщение
+                        $vk = new VKApiClient('5.103');
+                        $response = $vk->messages()->send(getenv('VK_TOKEN'), array(
+                            'user_id' => $user_id,
+                            'message' => $message,
+                            'keyboard' => json_encode($send_value_keyboard),
+                            'random_id' => rand(),
+                        ));
+                        echo 'ok';
+                        break;
 
                     } catch (\VK\Exceptions\VKApiException $e){
                         $this -> getlog($e -> getMessage());
