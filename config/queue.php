f33644e07a29d5cd8564430815f42297795da600
VladislavNep <VladislavNep@yandex.ru>
2019-11-15 10:38:21 +0700

    test

--- config/queue.php
+++ config/queue.php
@@ -35,7 +35,7 @@ return [
         ],
 
         'database' => [
-            'driver' => 'database',
+            'driver' => 'pgsql',
             'table' => 'jobs',
             'queue' => 'default',
             'retry_after' => 90,
