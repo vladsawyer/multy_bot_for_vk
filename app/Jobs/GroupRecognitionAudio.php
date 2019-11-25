5e73eebf1bd3820e705b92410cccf1d6ff224c8c
VladislavNep <VladislavNep@yandex.ru>
2019-11-25 12:39:39 +0700

    fix

--- app/Jobs/GroupRecognitionAudio.php
+++ app/Jobs/GroupRecognitionAudio.php
@@ -47,6 +47,7 @@ class GroupRecognitionAudio implements ShouldQueue
     }
 
     function download_audio_message($audio_file, $file_path ){
+        $this -> getlog($file_path);
 //        $audio_file_path = fopen( $file_path, 'w+b');
 //        $ch = curl_init($audio_file);
 //        curl_setopt($ch, CURLOPT_FILE, $audio_file_path);
@@ -62,6 +63,7 @@ class GroupRecognitionAudio implements ShouldQueue
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
         $data = curl_exec($ch);
         curl_close($ch);
@@ -71,12 +73,15 @@ class GroupRecognitionAudio implements ShouldQueue
 
     // отправка в yandex SpeechKit на распознование речи
     function send_speechKit_recognition($file_path){
-        $audio_file_path = file_get_contents($file_path);
+        $this -> getlog($file_path);
+        $audio_file_path = fopen( $file_path, 'rb');
+        $this -> getlog($audio_file_path);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?lang=ru-RU&format=oggopus");
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Api-Key ' . getenv('YANDEX_API_TOKEN')));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
